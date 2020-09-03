53efc968828d3276e94ef8fffbd5acb26ef65a60
swingsource <swingsource@163.com>
2020-09-03 22:21:35 +0800

    成绩单模块

---
+++ comment/index.js
@@ -0,0 +1,13 @@
+var dom1 = '<div class="comment-wrapper">' +
+    '<div class="title">成绩单</div>' +
+    '<div class="score-row">' +
+    '<div class="score-title">成绩：</div>' +
+    '<div class="score-content">'
+var dom2 = '</div>' +
+    '</div>' +
+    '<div class="cmt-row">' +
+    '<div class="cmt-title">点评：</div>' +
+    '<div class="cmt-content">'
+var dom3 = '</div>' +
+    '</div>' +
+    '</div>'
